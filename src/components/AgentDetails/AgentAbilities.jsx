988ad77e5f05dea63ec56e11558656976bc4e9ed
Karnveer Grewal <karnveergrewal4@gmail.com>
2023-04-10 11:13:44 -0400

    rendered and styled AgentAbilities

--- src/components/AgentDetails/AgentAbilities.jsx
+++ src/components/AgentDetails/AgentAbilities.jsx
@@ -1,6 +1,24 @@
+import { useState } from 'react';
+
 const AgentAbilities = ({ abilities }) => {
+  const [isAbilityDesc, setIsAbilityDesc] = useState(false);
+
   console.log(abilities);
-  return <h1>abiltiies</h1>;
+  const abilitiesList = abilities.map((ability, index) => (
+    <div className='ability' key={index}>
+      <div className='ability-header'>
+        <h4>{ability.slot}</h4>
+        <h3>{ability.displayName}</h3>
+        <img
+          src={ability.displayIcon}
+          alt={ability.displayName}
+          className='ability-icon'
+        />
+      </div>
+      <p className='ability-content'>{ability.description}</p>
+    </div>
+  ));
+  return <div className='ability-container'>{abilitiesList}</div>;
 };
 
 export default AgentAbilities;
